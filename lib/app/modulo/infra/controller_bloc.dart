5af1dd236168b8b7b5a571af88f2e4a1424b2de4
Nilton Carlos <niltonfirminofs@gmail.com>
2021-10-11 19:38:08 -0300

    Initial GetX

--- lib/app/modulo/infra/controller_bloc.dart
+++ lib/app/modulo/infra/controller_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobx_bloc_getx_cleandart/app/modulo/domain/use_case.dart';
+
 final nc = NumberCounter();
+
 class CounterCubit extends Cubit<int> {
   
   CounterCubit() : super(nc.number);
